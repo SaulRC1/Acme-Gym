779ccfe8e7bf82d91da165d20b66874f67f2979b
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2023-04-22 19:03:24 +0200

    Añadimos relación entre administrador y gimnasio

--- src/main/java/domain/Admin.java
+++ src/main/java/domain/Admin.java
@@ -6,32 +6,40 @@ import java.util.Collection;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 @Entity
 @Access(AccessType.PROPERTY)
 public class Admin extends Actor {
 
-	// Relationships
-	private Collection<Manager> managers;
+    // Relationships
+    private Collection<Manager> managers;
+    private Gym gym;
 
-	@OneToMany(mappedBy = "admin")
-	public Collection<Manager> getManagers() {
-	    return managers;
-	}
+    @OneToMany(mappedBy = "admin")
+    public Collection<Manager> getManagers() {
+	return this.managers;
+    }
 
-	public void setManagers(Collection<Manager> managers) {
-	    this.managers = managers;
-	}
+    public void setManagers(final Collection<Manager> managers) {
+	this.managers = managers;
+    }
 
-	public Admin(String firstName, String lastName, String address, String email, String phoneNumber,
-		String postalCode, String city, String country, Collection<Annotation> annotations,
-		Collection<Manager> managers) {
-	    super(firstName, lastName, address, email, phoneNumber, postalCode, city, country, annotations);
-	    this.managers = managers;
-	}
+    @ManyToOne(optional = false)
+    public Gym getGym() {
+	return this.gym;
+    }
 
-	
+    public void setGym(final Gym gym) {
+	this.gym = gym;
+    }
 
-	
+    public Admin(final String firstName, final String lastName, final String address, final String email,
+	    final String phoneNumber, final String postalCode, final String city, final String country,
+	    final Collection<Annotation> annotations, final Collection<Manager> managers) {
+	super(firstName, lastName, address, email, phoneNumber, postalCode, city, country, annotations);
+	this.managers = managers;
+    }
+
 }
--- src/main/java/domain/Gym.java
+++ src/main/java/domain/Gym.java
@@ -17,128 +17,138 @@ import org.hibernate.validator.constraints.URL;
 
 @Entity
 @Access(AccessType.PROPERTY)
-public class Gym  extends DomainEntity{
+public class Gym extends DomainEntity {
 
-	private String logo;
-	private String address;
-	private String name;
-	private Double fee;
-	private boolean active;
+    private String logo;
+    private String address;
+    private String name;
+    private Double fee;
+    private boolean active;
 
-	// Relationships
-	private Manager manager;
-	private Collection<Trainer> trainers;
-	private Collection<Activity> activities;
-	private Collection<Inscription> inscriptions;
-	private Collection<Annotation> annotations;
-	private Collection<Training> trainings;
+    // Relationships
+    private Manager manager;
+    private Collection<Trainer> trainers;
+    private Collection<Activity> activities;
+    private Collection<Inscription> inscriptions;
+    private Collection<Annotation> annotations;
+    private Collection<Training> trainings;
+    private Collection<Admin> administrators;
 
-	public Gym(final String logo, final String address, final String name, final Double fee, final boolean active) {
-		super();
-		this.logo = logo;
-		this.address = address;
-		this.name = name;
-		this.fee = fee;
-		this.active = active;
-	}
+    public Gym(final String logo, final String address, final String name, final Double fee, final boolean active) {
+	super();
+	this.logo = logo;
+	this.address = address;
+	this.name = name;
+	this.fee = fee;
+	this.active = active;
+    }
 
-	@OneToMany(mappedBy = "gym")
-	public Collection<Activity> getActivities() {
-		return this.activities;
-	}
+    @OneToMany(mappedBy = "gym")
+    public Collection<Activity> getActivities() {
+	return this.activities;
+    }
 
-	@NotBlank
-	public String getAddress() {
-		return this.address;
-	}
+    @NotBlank
+    public String getAddress() {
+	return this.address;
+    }
 
-	@OneToMany(mappedBy = "gym")
-	public Collection<Annotation> getAnnotations() {
-		return this.annotations;
-	}
+    @OneToMany(mappedBy = "gym")
+    public Collection<Annotation> getAnnotations() {
+	return this.annotations;
+    }
 
-	@Min(0)
-	@Digits(integer = 9, fraction = 2)
-	public Double getFee() {
-		return this.fee;
-	}
+    @Min(0)
+    @Digits(integer = 9, fraction = 2)
+    public Double getFee() {
+	return this.fee;
+    }
 
-	@OneToMany(mappedBy = "gym")
-	public Collection<Inscription> getInscriptions() {
-		return this.inscriptions;
-	}
+    @OneToMany(mappedBy = "gym")
+    public Collection<Inscription> getInscriptions() {
+	return this.inscriptions;
+    }
 
-	@URL
-	public String getLogo() {
-		return this.logo;
-	}
+    @URL
+    public String getLogo() {
+	return this.logo;
+    }
 
-	@ManyToOne(optional = false)
-	public Manager getManager() {
-		return this.manager;
-	}
+    @ManyToOne(optional = false)
+    public Manager getManager() {
+	return this.manager;
+    }
 
-	@NotBlank
-	public String getName() {
-		return this.name;
-	}
+    @NotBlank
+    public String getName() {
+	return this.name;
+    }
 
-	@OneToMany(mappedBy = "gym")
-	public Collection<Trainer> getTrainers() {
-		return this.trainers;
-	}
+    @OneToMany(mappedBy = "gym")
+    public Collection<Trainer> getTrainers() {
+	return this.trainers;
+    }
 
-	@OneToMany(mappedBy = "gym")
-	public Collection<Training> getTrainings() {
-		return this.trainings;
-	}
+    @OneToMany(mappedBy = "gym")
+    public Collection<Training> getTrainings() {
+	return this.trainings;
+    }
 
-	public boolean isActive() {
-		return this.active;
-	}
+    public boolean isActive() {
+	return this.active;
+    }
 
-	public void setActive(final boolean active) {
-		this.active = active;
-	}
+    public void setActive(final boolean active) {
+	this.active = active;
+    }
 
-	public void setActivities(final List<Activity> activities) {
-		this.activities = activities;
-	}
+    public void setActivities(final List<Activity> activities) {
+	this.activities = activities;
+    }
 
-	public void setAddress(final String address) {
-		this.address = address;
-	}
+    public void setAddress(final String address) {
+	this.address = address;
+    }
 
-	public void setAnnotations(final List<Annotation> annotations) {
-		this.annotations = annotations;
-	}
+    public void setAnnotations(final List<Annotation> annotations) {
+	this.annotations = annotations;
+    }
 
-	public void setFee(final Double fee) {
-		this.fee = fee;
-	}
+    public void setFee(final Double fee) {
+	this.fee = fee;
+    }
 
-	public void setInscriptions(final List<Inscription> inscriptions) {
-		this.inscriptions = inscriptions;
-	}
+    public void setInscriptions(final List<Inscription> inscriptions) {
+	this.inscriptions = inscriptions;
+    }
 
-	public void setLogo(final String logo) {
-		this.logo = logo;
-	}
+    public void setLogo(final String logo) {
+	this.logo = logo;
+    }
 
-	public void setManager(final Manager manager) {
-		this.manager = manager;
-	}
+    public void setManager(final Manager manager) {
+	this.manager = manager;
+    }
 
-	public void setName(final String name) {
-		this.name = name;
-	}
+    public void setName(final String name) {
+	this.name = name;
+    }
 
-	public void setTrainers(final List<Trainer> trainers) {
-		this.trainers = trainers;
-	}
+    public void setTrainers(final List<Trainer> trainers) {
+	this.trainers = trainers;
+    }
 
-	public void setTrainings(final List<Training> trainings) {
-		this.trainings = trainings;
-	}
+    public void setTrainings(final List<Training> trainings) {
+	this.trainings = trainings;
+    }
+
+    @OneToMany(mappedBy = "gym")
+    public Collection<Admin> getAdministrators() {
+	return this.administrators;
+    }
+
+    public void setAdministrators(final Collection<Admin> administrators) {
+	this.administrators = administrators;
+    }
 
 }
