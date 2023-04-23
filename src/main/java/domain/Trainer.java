df711727d6f134be0a2c1bcb890e66422e1b1008
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2023-04-23 20:34:55 +0200

    Relación muchos a muchos entre Actividad y Entrenador

--- src/main/java/domain/Trainer.java
+++ src/main/java/domain/Trainer.java
@@ -1,10 +1,12 @@
 
 package domain;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
@@ -14,45 +16,54 @@ import javax.persistence.OneToOne;
 @Access(AccessType.PROPERTY)
 public class Trainer extends Actor {
 
-	// Relationships
-	private Curriculum curriculum;
-	private Gym gym;
-	private Activity activity;
+    // Relationships
+    private Curriculum curriculum;
+    private Gym gym;
+    private Collection<Activity> activities;
 
-	public Trainer(String firstName, String lastName, String address, String email, String phoneNumber,
-			String postalCode, String city, String country, Collection<Annotation> annotations, Curriculum curriculum,
-			Gym gym, Activity activity) {
-		super(firstName, lastName, address, email, phoneNumber, postalCode, city, country, annotations);
-		this.curriculum = curriculum;
-		this.gym = gym;
-		this.activity = activity;
-	}
+    public Trainer(String firstName, String lastName, String address, String email, String phoneNumber,
+	    String postalCode, String city, String country, Collection<Annotation> annotations, Curriculum curriculum,
+	    Gym gym, Collection<Activity> activities) {
+	super(firstName, lastName, address, email, phoneNumber, postalCode, city, country, annotations);
+	this.curriculum = curriculum;
+	this.gym = gym;
+	this.activities = activities;
+    }
 
-	@ManyToMany
-	public Activity getActivity() {
-		return this.activity;
-	}
+    @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    public Collection<Activity> getActivities() {
+	return this.activities;
+    }
 
-	@OneToOne(optional = false)
-	public Curriculum getCurriculum() {
-		return this.curriculum;
-	}
+    public void setActivities(Collection<Activity> activities) {
+	this.activities = activities;
+    }
 
-	@ManyToOne(optional = false)
-	public Gym getGym() {
-		return this.gym;
-	}
+    public void addActivity(Activity activity) {
 
-	public void setActivity(final Activity activity) {
-		this.activity = activity;
-	}
+	if (this.activities == null)
+	    this.activities = new ArrayList<Activity>();
 
-	public void setCurriculum(final Curriculum curriculum) {
-		this.curriculum = curriculum;
-	}
+	this.activities.add(activity);
+	activity.getTrainers().add(this);
+    }
 
-	public void setGym(final Gym gym) {
-		this.gym = gym;
-	}
+    @OneToOne(optional = false)
+    public Curriculum getCurriculum() {
+	return this.curriculum;
+    }
+
+    @ManyToOne(optional = false)
+    public Gym getGym() {
+	return this.gym;
+    }
+
+    public void setCurriculum(final Curriculum curriculum) {
+	this.curriculum = curriculum;
+    }
+
+    public void setGym(final Gym gym) {
+	this.gym = gym;
+    }
 
 }
