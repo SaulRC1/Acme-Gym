e9e1fbacacd8b0ca65e0d2bfdea7a002d79b74de
Manuel Cerrejon <manucrjon@gmail.com>
2023-05-16 00:35:19 +0200

    Corrección de fallos del Populate, falta solucionar el ciclo de generación de beans.

--- src/main/java/domain/Activity.java
+++ src/main/java/domain/Activity.java
@@ -26,299 +26,301 @@ import org.hibernate.validator.constraints.URL;
 @Access(AccessType.PROPERTY)
 public class Activity extends DomainEntity {
 
-    private String title;
-    private Collection<String> photos;
-    private String description;
-    private Set<DayOfWeek> daysOfWeek;
-    private LocalTime startHour;
-    private LocalTime endHour;
-    private Integer availablePlaces;
+	private String					title;
+	private String					photo;
+	private String					description;
+	private Set<DayOfWeek>			daysOfWeek;
+	private LocalTime				startHour;
+	private LocalTime				endHour;
+	private Integer					availablePlaces;
 
-    // Relationships
-    private Gym gym;
-    private Collection<Trainer> trainers;
-    private Collection<Annotation> annotations;
-    private Collection<Client> clients;
+	// Relationships
+	private Gym						gym;
+	private Collection<Trainer>		trainers;
+	private Collection<Annotation>	annotations;
+	private Collection<Client>		clients;
 
-    public Activity(final String tittle, final Collection<String> photos, final String description,
-	    final Set<DayOfWeek> daysOfWeek, final LocalTime startHour, final LocalTime endHour,
-	    final Integer availablePlaces) {
-	super();
-	this.title = tittle;
-	this.photos = photos;
-	this.description = description;
-	this.daysOfWeek = daysOfWeek;
-	this.startHour = startHour;
-	this.endHour = endHour;
-	this.availablePlaces = availablePlaces;
-    }
 
-    public Activity() {
-
-    }
-
-    @OneToMany(mappedBy = "activity", cascade = CascadeType.ALL, orphanRemoval = true)
-    public Collection<Annotation> getAnnotations() {
-	return this.annotations;
-    }
-
-    @NotNull
-    @Min(0) // Comprobar
-    public Integer getAvailablePlaces() {
-	return this.availablePlaces;
-    }
-
-    @ManyToMany(mappedBy = "activities")
-    public Collection<Client> getClients() {
-	return this.clients;
-    }
-
-    @NotBlank
-    public String getDescription() {
-	return this.description;
-    }
-
-    @NotNull
-    public LocalTime getEndHour() {
-	return this.endHour;
-    }
-
-    @ManyToOne(optional = false)
-    public Gym getGym() {
-	return this.gym;
-    }
-
-    @URL
-    @ElementCollection
-    public Collection<String> getPhotos() {
-	return this.photos;
-    }
-
-    @NotNull
-    public LocalTime getStartHour() {
-	return this.startHour;
-    }
-
-    @NotBlank
-    public String getTitle() {
-	return this.title;
-    }
-
-    @ManyToMany(mappedBy = "activities")
-    @NotEmpty
-    public Collection<Trainer> getTrainers() {
-	return this.trainers;
-    }
-
-    @NotEmpty
-    @ElementCollection
-    public Set<DayOfWeek> getDaysOfWeek() {
-	return this.daysOfWeek;
-    }
-
-    public void setAnnotations(final Collection<Annotation> annotations) {
-	this.annotations = annotations;
-    }
-
-    public void setAvailablePlaces(final Integer availablePlaces) {
-	this.availablePlaces = availablePlaces;
-    }
-
-    public void setClients(final Collection<Client> clients) {
-	this.clients = clients;
-    }
-
-    public void setDescription(final String description) {
-	this.description = description;
-    }
-
-    public void setEndHour(final LocalTime endHour) {
-	this.endHour = endHour;
-    }
-
-    public void setGym(final Gym gym) {
-	this.gym = gym;
-    }
-
-    public void setPhotos(final Collection<String> photos) {
-	this.photos = photos;
-    }
-
-    public void setStartHour(final LocalTime startHour) {
-	this.startHour = startHour;
-    }
-
-    public void setTitle(final String title) {
-	this.title = title;
-    }
-
-    public void setTrainers(final Collection<Trainer> trainers) {
-	this.trainers = trainers;
-    }
-
-    public void setDaysOfWeek(final Set<DayOfWeek> daysOfWeek) {
-	this.daysOfWeek = daysOfWeek;
-    }
-
-    @Override
-    public int hashCode() {
-	final int prime = 31;
-	int result = super.hashCode();
-	result = prime * result + ((this.annotations == null) ? 0 : this.annotations.hashCode());
-	result = prime * result + ((this.availablePlaces == null) ? 0 : this.availablePlaces.hashCode());
-	result = prime * result + ((this.clients == null) ? 0 : this.clients.hashCode());
-	result = prime * result + ((this.daysOfWeek == null) ? 0 : this.daysOfWeek.hashCode());
-	result = prime * result + ((this.description == null) ? 0 : this.description.hashCode());
-	result = prime * result + ((this.endHour == null) ? 0 : this.endHour.hashCode());
-	result = prime * result + ((this.gym == null) ? 0 : this.gym.hashCode());
-	result = prime * result + ((this.photos == null) ? 0 : this.photos.hashCode());
-	result = prime * result + ((this.startHour == null) ? 0 : this.startHour.hashCode());
-	result = prime * result + ((this.title == null) ? 0 : this.title.hashCode());
-	result = prime * result + ((this.trainers == null) ? 0 : this.trainers.hashCode());
-	return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (this.getClass() != obj.getClass())
-	    return false;
-	Activity other = (Activity) obj;
-	if (this.annotations == null) {
-	    if (other.annotations != null)
-		return false;
-	} else if (!this.annotations.equals(other.annotations))
-	    return false;
-	if (this.availablePlaces == null) {
-	    if (other.availablePlaces != null)
-		return false;
-	} else if (!this.availablePlaces.equals(other.availablePlaces))
-	    return false;
-	if (this.clients == null) {
-	    if (other.clients != null)
-		return false;
-	} else if (!this.clients.equals(other.clients))
-	    return false;
-	if (this.daysOfWeek == null) {
-	    if (other.daysOfWeek != null)
-		return false;
-	} else if (!this.daysOfWeek.equals(other.daysOfWeek))
-	    return false;
-	if (this.description == null) {
-	    if (other.description != null)
-		return false;
-	} else if (!this.description.equals(other.description))
-	    return false;
-	if (this.endHour == null) {
-	    if (other.endHour != null)
-		return false;
-	} else if (!this.endHour.equals(other.endHour))
-	    return false;
-	if (this.gym == null) {
-	    if (other.gym != null)
-		return false;
-	} else if (!this.gym.equals(other.gym))
-	    return false;
-	if (this.photos == null) {
-	    if (other.photos != null)
-		return false;
-	} else if (!this.photos.equals(other.photos))
-	    return false;
-	if (this.startHour == null) {
-	    if (other.startHour != null)
-		return false;
-	} else if (!this.startHour.equals(other.startHour))
-	    return false;
-	if (this.title == null) {
-	    if (other.title != null)
-		return false;
-	} else if (!this.title.equals(other.title))
-	    return false;
-	if (this.trainers == null) {
-	    if (other.trainers != null)
-		return false;
-	} else if (!this.trainers.equals(other.trainers))
-	    return false;
-	return true;
-    }
-
-    /**
-     * Will add a trainer to train this activity.<br>
-     * <br>
-     *
-     * DO NOT CALL THIS METHOD. TO PROPERLY ADD A TRAINER TO AN ACTIVITY, USE METHOD
-     * {@linkplain Trainer#addActivity(Activity)}}.
-     *
-     * @param trainer The trainer to be added.
-     */
-    public void addTrainer(Trainer trainer) {
-
-	if (trainer != null) {
-
-	    if (this.trainers == null)
-		this.trainers = new ArrayList<Trainer>();
-
-	    this.trainers.add(trainer);
+	public Activity(final String tittle, final String photo, final String description, final Set<DayOfWeek> daysOfWeek, final LocalTime startHour, final LocalTime endHour, final Integer availablePlaces) {
+		super();
+		this.title = tittle;
+		this.photo = photo;
+		this.description = description;
+		this.daysOfWeek = daysOfWeek;
+		this.startHour = startHour;
+		this.endHour = endHour;
+		this.availablePlaces = availablePlaces;
 	}
-    }
 
-    /**
-     * Will add a client to this activity.<br>
-     * <br>
-     *
-     * DO NOT CALL THIS METHOD. TO PROPERLY ADD A CLIENT TO AN ACTIVITY, USE METHOD
-     * {@linkplain Client#addActivity(Activity)}}.
-     *
-     * @param client The client to be added.
-     */
-    public void addClient(Client client) {
+	public Activity() {
 
-	if (client != null) {
-
-	    if (this.clients == null)
-		this.clients = new ArrayList<Client>();
-
-	    this.clients.add(client);
 	}
-    }
 
-    /**
-     * Will add an annotation to this activity.<br>
-     * <br>
-     *
-     * NOTE THAT CALLING THIS METHOD WILL ALSO ADD THIS ACTIVITY TO THE ANNOTATION
-     * PASSED BY PARAMETER.
-     *
-     * @param annotation The annotation to be added.
-     */
-    public void addAnnotation(Annotation annotation) {
-
-	if (annotation != null) {
-
-	    if (this.annotations == null)
-		this.annotations = new ArrayList<Annotation>();
-
-	    this.annotations.add(annotation);
-	    annotation.setActivity(this);
+	@OneToMany(mappedBy = "activity", cascade = CascadeType.ALL, orphanRemoval = true)
+	public Collection<Annotation> getAnnotations() {
+		return this.annotations;
 	}
-    }
 
-    /**
-     * Will remove an annotation from this activity.<br>
-     * <br>
-     *
-     * NOTE THAT CALLING THIS METHOD WILL ALSO REMOVE THIS ACTIVITY FROM THE
-     * ANNOTATION PASSED BY PARAMETER.
-     *
-     * @param annotation The annotation to be removed.
-     */
-    public void removeAnnotation(Annotation annotation) {
-
-	if (annotation != null && this.annotations != null) {
-	    this.annotations.remove(annotation);
-	    annotation.setActivity(null);
+	@NotNull
+	@Min(0) // Comprobar
+	public Integer getAvailablePlaces() {
+		return this.availablePlaces;
 	}
-    }
+
+	@ManyToMany(mappedBy = "activities")
+	public Collection<Client> getClients() {
+		return this.clients;
+	}
+
+	@NotBlank
+	public String getDescription() {
+		return this.description;
+	}
+
+	@NotNull
+	public LocalTime getEndHour() {
+		return this.endHour;
+	}
+
+	@ManyToOne(optional = false)
+	public Gym getGym() {
+		return this.gym;
+	}
+
+	@URL
+	public String getPhoto() {
+		return this.photo;
+	}
+
+	@NotNull
+	public LocalTime getStartHour() {
+		return this.startHour;
+	}
+
+	@NotBlank
+	public String getTitle() {
+		return this.title;
+	}
+
+	@ManyToMany(mappedBy = "activities")
+	@NotEmpty
+	public Collection<Trainer> getTrainers() {
+		return this.trainers;
+	}
+
+	@NotEmpty
+	@ElementCollection
+	public Set<DayOfWeek> getDaysOfWeek() {
+		return this.daysOfWeek;
+	}
+
+	public void setAnnotations(final Collection<Annotation> annotations) {
+		this.annotations = annotations;
+	}
+
+	public void setAvailablePlaces(final Integer availablePlaces) {
+		this.availablePlaces = availablePlaces;
+	}
+
+	public void setClients(final Collection<Client> clients) {
+		this.clients = clients;
+	}
+
+	public void setDescription(final String description) {
+		this.description = description;
+	}
+
+	public void setEndHour(final LocalTime endHour) {
+		this.endHour = endHour;
+	}
+
+	public void setGym(final Gym gym) {
+		this.gym = gym;
+	}
+
+	public void setPhoto(final String photo) {
+		this.photo = photo;
+	}
+
+	public void setStartHour(final LocalTime startHour) {
+		this.startHour = startHour;
+	}
+
+	public void setTitle(final String title) {
+		this.title = title;
+	}
+
+	public void setTrainers(final Collection<Trainer> trainers) {
+		this.trainers = trainers;
+	}
+
+	public void setDaysOfWeek(final Set<DayOfWeek> daysOfWeek) {
+		this.daysOfWeek = daysOfWeek;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((this.annotations == null) ? 0 : this.annotations.hashCode());
+		result = prime * result + ((this.availablePlaces == null) ? 0 : this.availablePlaces.hashCode());
+		result = prime * result + ((this.clients == null) ? 0 : this.clients.hashCode());
+		result = prime * result + ((this.daysOfWeek == null) ? 0 : this.daysOfWeek.hashCode());
+		result = prime * result + ((this.description == null) ? 0 : this.description.hashCode());
+		result = prime * result + ((this.endHour == null) ? 0 : this.endHour.hashCode());
+		result = prime * result + ((this.gym == null) ? 0 : this.gym.hashCode());
+		result = prime * result + ((this.photo == null) ? 0 : this.photo.hashCode());
+		result = prime * result + ((this.startHour == null) ? 0 : this.startHour.hashCode());
+		result = prime * result + ((this.title == null) ? 0 : this.title.hashCode());
+		result = prime * result + ((this.trainers == null) ? 0 : this.trainers.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (this.getClass() != obj.getClass())
+			return false;
+		final Activity other = (Activity) obj;
+		if (this.annotations == null) {
+			if (other.annotations != null)
+				return false;
+		} else if (!this.annotations.equals(other.annotations))
+			return false;
+		if (this.availablePlaces == null) {
+			if (other.availablePlaces != null)
+				return false;
+		} else if (!this.availablePlaces.equals(other.availablePlaces))
+			return false;
+		if (this.clients == null) {
+			if (other.clients != null)
+				return false;
+		} else if (!this.clients.equals(other.clients))
+			return false;
+		if (this.daysOfWeek == null) {
+			if (other.daysOfWeek != null)
+				return false;
+		} else if (!this.daysOfWeek.equals(other.daysOfWeek))
+			return false;
+		if (this.description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!this.description.equals(other.description))
+			return false;
+		if (this.endHour == null) {
+			if (other.endHour != null)
+				return false;
+		} else if (!this.endHour.equals(other.endHour))
+			return false;
+		if (this.gym == null) {
+			if (other.gym != null)
+				return false;
+		} else if (!this.gym.equals(other.gym))
+			return false;
+		if (this.photo == null) {
+			if (other.photo != null)
+				return false;
+		} else if (!this.photo.equals(other.photo))
+			return false;
+		if (this.startHour == null) {
+			if (other.startHour != null)
+				return false;
+		} else if (!this.startHour.equals(other.startHour))
+			return false;
+		if (this.title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!this.title.equals(other.title))
+			return false;
+		if (this.trainers == null) {
+			if (other.trainers != null)
+				return false;
+		} else if (!this.trainers.equals(other.trainers))
+			return false;
+		return true;
+	}
+
+	/**
+	 * Will add a trainer to train this activity.<br>
+	 * <br>
+	 *
+	 * DO NOT CALL THIS METHOD. TO PROPERLY ADD A TRAINER TO AN ACTIVITY, USE METHOD
+	 * {@linkplain Trainer#addActivity(Activity)}}.
+	 *
+	 * @param trainer
+	 *            The trainer to be added.
+	 */
+	public void addTrainer(final Trainer trainer) {
+
+		if (trainer != null) {
+
+			if (this.trainers == null)
+				this.trainers = new ArrayList<Trainer>();
+
+			this.trainers.add(trainer);
+		}
+	}
+
+	/**
+	 * Will add a client to this activity.<br>
+	 * <br>
+	 *
+	 * DO NOT CALL THIS METHOD. TO PROPERLY ADD A CLIENT TO AN ACTIVITY, USE METHOD
+	 * {@linkplain Client#addActivity(Activity)}}.
+	 *
+	 * @param client
+	 *            The client to be added.
+	 */
+	public void addClient(final Client client) {
+
+		if (client != null) {
+
+			if (this.clients == null)
+				this.clients = new ArrayList<Client>();
+
+			this.clients.add(client);
+		}
+	}
+
+	/**
+	 * Will add an annotation to this activity.<br>
+	 * <br>
+	 *
+	 * NOTE THAT CALLING THIS METHOD WILL ALSO ADD THIS ACTIVITY TO THE ANNOTATION
+	 * PASSED BY PARAMETER.
+	 *
+	 * @param annotation
+	 *            The annotation to be added.
+	 */
+	public void addAnnotation(final Annotation annotation) {
+
+		if (annotation != null) {
+
+			if (this.annotations == null)
+				this.annotations = new ArrayList<Annotation>();
+
+			this.annotations.add(annotation);
+			annotation.setActivity(this);
+		}
+	}
+
+	/**
+	 * Will remove an annotation from this activity.<br>
+	 * <br>
+	 *
+	 * NOTE THAT CALLING THIS METHOD WILL ALSO REMOVE THIS ACTIVITY FROM THE
+	 * ANNOTATION PASSED BY PARAMETER.
+	 *
+	 * @param annotation
+	 *            The annotation to be removed.
+	 */
+	public void removeAnnotation(final Annotation annotation) {
+
+		if (annotation != null && this.annotations != null) {
+			this.annotations.remove(annotation);
+			annotation.setActivity(null);
+		}
+	}
 
 }
--- src/main/java/domain/SocialIdentity.java
+++ src/main/java/domain/SocialIdentity.java
@@ -6,88 +6,87 @@ import javax.persistence.Embeddable;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.URL;
 
-import domain.enums.SocialMedia;
-
 @Embeddable
 public class SocialIdentity {
 
-    private String nick;
-    private String link;
-    private String socialMedia;
+	private String	nick;
+	private String	link;
+	private String	socialMedia;
 
-    public SocialIdentity(final String nick, final String link, final String socialMedia) {
-	super();
-	this.nick = nick;
-	this.link = link;
-	this.socialMedia = socialMedia;
-    }
 
-    public SocialIdentity() {
+	public SocialIdentity(final String nick, final String link, final String socialMedia) {
+		super();
+		this.nick = nick;
+		this.link = link;
+		this.socialMedia = socialMedia;
+	}
 
-    }
+	public SocialIdentity() {
 
-    @URL
-    public String getLink() {
-	return this.link;
-    }
+	}
 
-    @NotBlank
-    public String getNick() {
-	return this.nick;
-    }
+	@URL
+	public String getLink() {
+		return this.link;
+	}
 
-    @NotBlank
-    public String getSocialMedia() {
-	return this.socialMedia;
-    }
+	@NotBlank
+	public String getNick() {
+		return this.nick;
+	}
 
-    public void setLink(final String link) {
-	this.link = link;
-    }
+	@NotBlank
+	public String getSocialMedia() {
+		return this.socialMedia;
+	}
 
-    public void setNick(final String nick) {
-	this.nick = nick;
-    }
+	public void setLink(final String link) {
+		this.link = link;
+	}
 
-    public void setSocialMedia(SocialMedia socialMedia) {
-	this.socialMedia = socialMedia.getSocialMediaName();
-    }
+	public void setNick(final String nick) {
+		this.nick = nick;
+	}
 
-    @Override
-    public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((this.link == null) ? 0 : this.link.hashCode());
-	result = prime * result + ((this.nick == null) ? 0 : this.nick.hashCode());
-	result = prime * result + ((this.socialMedia == null) ? 0 : this.socialMedia.hashCode());
-	return result;
-    }
+	public void setSocialMedia(final String socialMedia) {
+		this.socialMedia = socialMedia;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (obj == null)
-	    return false;
-	if (this.getClass() != obj.getClass())
-	    return false;
-	SocialIdentity other = (SocialIdentity) obj;
-	if (this.link == null) {
-	    if (other.link != null)
-		return false;
-	} else if (!this.link.equals(other.link))
-	    return false;
-	if (this.nick == null) {
-	    if (other.nick != null)
-		return false;
-	} else if (!this.nick.equals(other.nick))
-	    return false;
-	if (this.socialMedia == null) {
-	    if (other.socialMedia != null)
-		return false;
-	} else if (!this.socialMedia.equals(other.socialMedia))
-	    return false;
-	return true;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.link == null) ? 0 : this.link.hashCode());
+		result = prime * result + ((this.nick == null) ? 0 : this.nick.hashCode());
+		result = prime * result + ((this.socialMedia == null) ? 0 : this.socialMedia.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (this.getClass() != obj.getClass())
+			return false;
+		final SocialIdentity other = (SocialIdentity) obj;
+		if (this.link == null) {
+			if (other.link != null)
+				return false;
+		} else if (!this.link.equals(other.link))
+			return false;
+		if (this.nick == null) {
+			if (other.nick != null)
+				return false;
+		} else if (!this.nick.equals(other.nick))
+			return false;
+		if (this.socialMedia == null) {
+			if (other.socialMedia != null)
+				return false;
+		} else if (!this.socialMedia.equals(other.socialMedia))
+			return false;
+		return true;
+	}
 
 }
